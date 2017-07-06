3a4b23217c22b0485efb8ae8e9b3d47a01dad88c
chandureddys <csreddy.512@gmail.com>
2017-07-06 19:40:14 -0400

    added Test class to test the DI

---
+++ src/main/java/com/core/spring/spring_core/Test.java
@@ -0,0 +1,16 @@
+package com.core.spring.spring_core;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Student student= (Student) context.getBean("student");
+		 
+		System.out.println("Employee ID: " + student.getId());
+		System.out.println("Employee Name: " + student.getName());
+	}
+
+}
