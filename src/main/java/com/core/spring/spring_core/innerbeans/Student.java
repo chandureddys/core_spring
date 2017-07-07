497d043159182cc0a25b21498952dc00d994fe0e
chandureddys <csreddy.512@gmail.com>
2017-07-07 10:39:47 -0400

    adding innerbeans classes

---
+++ src/main/java/com/core/spring/spring_core/innerbeans/Student.java
@@ -0,0 +1,30 @@
+package com.core.spring.spring_core.innerbeans;
+
+public class Student {
+	
+	private String name;
+	
+	private Address address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", address=" + address + "]";
+	}
+
+}
---
+++ src/main/java/com/core/spring/spring_core/innerbeans/Test.java
@@ -0,0 +1,15 @@
+package com.core.spring.spring_core.innerbeans;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/core/spring/spring_core/innerbeans/config.xml");
+		Student student= (Student) context.getBean("student");
+		System.out.println(student);
+	}
+
+}
