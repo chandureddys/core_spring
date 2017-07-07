1d7ee7025a34bf9e511753ac5b6a0fd4b40a233d
chandureddys <csreddy.512@gmail.com>
2017-07-07 08:32:19 -0400

    invoking destory method

--- src/main/java/com/ls/spring/spring_core/TestPatient.java
+++ src/main/java/com/ls/spring/spring_core/TestPatient.java
@@ -1,5 +1,6 @@
 package com.ls.spring.spring_core;
 
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.core.spring.spring_core.Student;
@@ -8,10 +9,12 @@ public class TestPatient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("patientconfig.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("patientconfig.xml");
 		Patient patient= (Patient) context.getBean("patient");
 		 
 		System.out.println(patient);
+		
+		context.registerShutdownHook();
 		
 	}
 
